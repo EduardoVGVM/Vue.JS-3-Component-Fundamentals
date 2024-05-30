1a0fc7b477d056439ef6fb42bf40c0f54b55394d
Eduardo Morais <dudu.vgvm@gmail.com>
2024-05-29 21:59:05 -0300

    CoffeePlanPicker

---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+    name: { type: String, default: 'Default Plan', validator(value) {
+        return value.startsWith("The")
+    }},
+    selected: { type: Boolean, default: false }
+})
+</script>
+
+<template>
+    <div class="plan" :class="{ 'active-plan': selected }">
+        <div class="description">
+            <span class="title"> {{ name }} </span>
+        </div>
+    </div>
+</template>
---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import CoffeePlan from './CoffeePlan.vue'
+
+const plans = ref(["The Single", "The Curious", "The Addict", "The Hacker"])
+
+const selectedCoffeePlan = ref()
+function handleSelectCoffePlan(name) {
+    selectedCoffeePlan.value = name
+}
+
+const plansWrapper = ref();
+</script>
+
+<template>
+    <div ref="plansWrapper" class="plans">
+        <CoffeePlan v-for="plan in plans" :key="plan" :name="plan" :selected="plan === selectedCoffeePlan"
+            @click="handleSelectCoffePlan(plan)">
+        </CoffeePlan>
+    </div>
+</template>
